6ce0ab7b2458ffbc18bdc4478338b1bae4490630
Gavinsta <gavin.kw.lau@gmail.com>
2022-11-22 16:07:43 -0700

    Adding Server redirect to ReactApp.

---
+++ WebApp/server/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const PORT = process.env.PORT || 8080;
+const app = (0, express_1.default)();
+dotenv_1.default.config();
+app.use((0, cors_1.default)());
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World From the Typescript Server!</h1>');
+});
+app.listen(PORT, () => {
+    console.log("Listening on port: " + PORT);
+});
--- WebApp/server/src/index.ts
+++ WebApp/server/src/index.ts
@@ -1,9 +1,20 @@
-import express from "express";
-const PORT = 8080;
-const app = express();
-app.get("/", (req, res) => {
-  return res.send("hello world");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
+
+const PORT = process.env.PORT || 8080;
+
+const app: Express = express();
+
+dotenv.config();
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Hello World From the Typescript Server!</h1>')
 });
+
 app.listen(PORT, () => {
   console.log("Listening on port: " + PORT);
 });
