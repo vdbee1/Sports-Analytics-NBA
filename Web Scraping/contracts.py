84576e4535eefb48863dd47e01d8d8997b663242
Gavinsta <gavin.kw.lau@gmail.com>
2022-11-27 14:10:45 -0700

    -Cleaned contracts data (removed '$'), and updated script slightly

--- Web Scraping/contracts.py
+++ Web Scraping/contracts.py
@@ -7,10 +7,21 @@ url = "https://www.basketball-reference.com/contracts/players.html"
 
 html = urlopen(url)
 soup = BeautifulSoup(html, features="html.parser")
-urls = soup.find_all('table',id='player-contracts')[0].findAll('tr')
-rows_data = [[td.getText() for td in urls[i].findAll('td')] for i in range(len(urls))]
+urls = soup.find_all('table', id='player-contracts')[0].findAll('tr')
+rows_data = [[td.getText() for td in urls[i].findAll('td')]
+             for i in range(len(urls))]
 
-fix=rows_data[1:]
-contracts= pd.DataFrame(fix, columns = ['Player', 'Tm', '2022-23', '2023-24', '2024-25', '2025-26', '2026-27', '2027-28', 'Guaranteed'])
-contracts.dropna(inplace = True) 
-contracts.to_csv("C:\Code\contracts.csv", index=False)
+fix = rows_data[1:]
+contracts = pd.DataFrame(fix, columns=[
+                         'PlayerName', 'Tm', '2022-23', '2023-24', '2024-25', '2025-26', '2026-27', '2027-28', 'Guaranteed'])
+
+contracts.dropna(inplace=True)
+colNames = ['2022-23', '2023-24', '2024-25',
+            '2025-26', '2026-27', '2027-28', 'Guaranteed']
+for colName in colNames:
+    contracts[colName] = contracts[colName].str.replace('$', '')
+    contracts[colName] = contracts[colName].str.replace(',', '')
+    contracts[colName] = pd.to_numeric(contracts[colName], errors='coerce')
+contracts.head(5)
+
+#contracts.to_csv("C:\Code\contracts.csv", index=False)
